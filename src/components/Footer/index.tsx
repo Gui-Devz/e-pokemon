17711b6510b15b5b44b6a4dcfdff8c733e245207
Guilherme <guilherme_devz@hotmail.com>
2021-10-03 21:03:32 -0300

    feat: creating the component Footer and its stylesheet

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+import { AiFillLinkedin } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
+
+import styles from "./footer.module.scss";
+
+export function Footer() {
+  return (
+    <div className={styles.supraContainer}>
+      <div className={styles.container}>
+        <div className={styles.copyright}>
+          <p>&copy; 2021 IZA.com.br</p>
+        </div>
+
+        <div className={styles.follow}>
+          <span>Follow us:</span>
+          <div className={styles.socialMedias}>
+            <Link href="https://www.linkedin.com/company/izaseguradora/">
+              <a target="_blank" rel="noopener">
+                <AiFillLinkedin size="2em" />
+              </a>
+            </Link>
+            <Link href="https://www.facebook.com/izacomvc">
+              <a target="_blank" rel="noopener">
+                <FaFacebookF size="1.4em" />
+              </a>
+            </Link>
+            <Link href="https://www.instagram.com/iza.com.vc/">
+              <a target="_blank" rel="noopener">
+                <AiOutlineInstagram size="2em" />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
