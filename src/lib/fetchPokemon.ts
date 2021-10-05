b721ddaed76342563e59239e9528757f0514c7ca
Guilherme <guilherme_devz@hotmail.com>
2021-10-05 14:32:57 -0300

    feat: moving the 'fetchPokemon' into the lib folder

---
+++ src/lib/fetchPokemon.ts
@@ -0,0 +1,21 @@
+const fetcher = (url) =>
+  fetch(url).then((res) => {
+    return res.json();
+  });
+
+const API_URL = "https://pokeapi.co/api/v2/pokemon";
+const PAGE_LIMIT = 10;
+
+export function fetchPokemon(name?: string, offset?: number): Promise<any> {
+  const uri = name
+    ? `${API_URL}/${name}`
+    : `${API_URL}?limit=${PAGE_LIMIT}&offset=${offset}`;
+
+  try {
+    const data = fetcher(uri);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
