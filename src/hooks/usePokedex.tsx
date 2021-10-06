365b32a2a1dead2863ce36decf7cd1f1b008c610
Guilherme <guilherme_devz@hotmail.com>
2021-10-06 11:59:44 -0300

    feat: creating the function 'fetchPokemonByName' and adding to the context values, there were more variables added to the context values such as 'favorites' and 'setFavoritesInPokedex'

--- src/hooks/usePokedex.tsx
+++ src/hooks/usePokedex.tsx
@@ -21,12 +21,11 @@ interface PokedexProviderProps {
 
 interface PokedexContextData {
   pokedex: PokemonProfile[];
-  memoizedFetchPokedex: (offset?: number) => Promise<void>;
+  favorites: PokemonProfile[];
+  fetchPokedex: (name?: string, offset?: number) => Promise<void>;
+  fetchPokemonByName: (name?: string) => Promise<void>;
   toggleFavorites: (pokemonProfile: PokemonProfile) => void;
-  /* fetchNextPokemonGroup: ()=> void;
-  addFavoritePokemon: ()=> void;
-  removeFavoritePokemon: ()=> void;
-  getFavoritePokemon: ()=> void; */
+  setFavoritesInPokedex: () => void;
 }
 
 const PokedexContext = createContext<PokedexContextData>(
@@ -37,60 +36,103 @@ export function PokedexProvider({ children }: PokedexProviderProps) {
   const [pokedex, setPokedex] = useState<PokemonProfile[]>([]);
   const [favorites, setFavorites] = useState<PokemonProfile[]>([]);
 
-  const memoizedFetchPokedex = useCallback(
-    async (offset?: number) => {
-      //fetching the names of the pokemons in groups of 10.
-      const data = await fetchPokemon("", offset);
-      const pokemonNames = data.results;
+  const fetchPokedex = async (name?: string, offset?: number) => {
+    //fetching the names of the pokemons in groups of 10.
+    const data = await fetchPokemon(name, offset);
+    const pokemonNames = data.results;
+    //getting the pokemon profile for each pokemon name from pokemonNames
+    const pokemonProfilesPromises = pokemonNames.map(async (pokemon) => {
+      const data = await fetchPokemon(pokemon.name);
+      //getting only the attributes that I want.
+      const rawAttributes = data?.stats.map((stat) => {
+        switch (stat.stat.name) {
+          case "hp":
+            return stat.base_stat;
 
-      //getting the pokemon profile for each pokemon name from pokemonNames
-      const pokemonProfilesPromises = pokemonNames.map(async (pokemon) => {
-        const data = await fetchPokemon(pokemon.name);
-        //getting only the attributes that I want.
-        const rawAttributes = data?.stats.map((stat) => {
-          switch (stat.stat.name) {
-            case "hp":
-              return stat.base_stat;
+          case "attack":
+            return stat.base_stat;
 
-            case "attack":
-              return stat.base_stat;
+          case "defense":
+            return stat.base_stat;
 
-            case "defense":
-              return stat.base_stat;
+          case "speed":
+            return stat.base_stat;
 
-            case "speed":
-              return stat.base_stat;
-
-            default:
-              break;
-          }
-        });
-        //removing the undefined from the array.
-        const attributes = rawAttributes.filter((att) => att !== undefined);
-
-        const pokemonProfile = {
-          name: pokemon.name,
-          pokemonImg: data?.sprites.other.dream_world.front_default,
-          attributes: attributes,
-          abilities: data?.abilities.map((val) => val.ability.name),
-        };
-
-        return pokemonProfile;
+          default:
+            break;
+        }
       });
+      //removing the undefined from the array.
+      const attributes = rawAttributes.filter((att) => att !== undefined);
 
-      const pokemonProfiles: PokemonProfile[] = await Promise.all(
-        pokemonProfilesPromises
-      );
+      const pokemonProfile = {
+        name: pokemon.name,
+        pokemonImg: data?.sprites.other.dream_world.front_default,
+        attributes: attributes,
+        abilities: data?.abilities.map((val) => val.ability.name),
+      };
 
-      //avoid duplicating pokedex.
-      if (pokedex[0]?.name === pokemonProfiles[0].name) {
-        return;
+      return pokemonProfile;
+    });
+
+    const pokemonProfiles: PokemonProfile[] = await Promise.all(
+      pokemonProfilesPromises
+    );
+
+    //avoid duplicating pokedex.
+    if (pokedex[0]?.name === pokemonProfiles[0].name) {
+      return;
+    }
+
+    //this code bock guarantees that there wil not be duplicated
+    //pokemon cards.
+    if (!offset) {
+      setPokedex([]);
+      setPokedex(pokemonProfiles);
+      return;
+    }
+    setPokedex([...pokedex, ...pokemonProfiles]);
+  };
+
+  const fetchPokemonByName = async (name: string) => {
+    const pokemon = await fetchPokemon(name, null);
+
+    //if there's no result with the name the user put, the pokedex
+    //will be set blank in order to show a message in the Pokedex component.
+    if (!pokemon) {
+      setPokedex([]);
+      return;
+    }
+    const rawAttributes = pokemon.stats.map((stat) => {
+      switch (stat.stat.name) {
+        case "hp":
+          return stat.base_stat;
+
+        case "attack":
+          return stat.base_stat;
+
+        case "defense":
+          return stat.base_stat;
+
+        case "speed":
+          return stat.base_stat;
+
+        default:
+          break;
       }
+    });
+    //removing the undefined from the array.
+    const attributes = rawAttributes.filter((att) => att !== undefined);
 
-      setPokedex([...pokedex, ...pokemonProfiles]);
-    },
-    [pokedex]
-  );
+    const pokemonProfile = {
+      name: pokemon.name,
+      pokemonImg: pokemon.sprites.other.dream_world.front_default,
+      attributes: attributes,
+      abilities: pokemon.abilities.map((val) => val.ability.name),
+    };
+
+    setPokedex([pokemonProfile]);
+  };
 
   const toggleFavorites = (pokemonProfile) => {
     //checking if pokemon is already in favorites.
@@ -110,13 +152,27 @@ export function PokedexProvider({ children }: PokedexProviderProps) {
     setFavorites([...favorites, pokemonProfile]);
   };
 
+  const setFavoritesInPokedex = () => {
+    if (favorites.length > 0) {
+      setPokedex(favorites);
+    }
+    return;
+  };
+
   useEffect(() => {
-    memoizedFetchPokedex();
-  }, [memoizedFetchPokedex]);
+    fetchPokedex();
+  }, []);
 
   return (
     <PokedexContext.Provider
-      value={{ pokedex, toggleFavorites, memoizedFetchPokedex }}
+      value={{
+        pokedex,
+        favorites,
+        toggleFavorites,
+        fetchPokedex,
+        fetchPokemonByName,
+        setFavoritesInPokedex,
+      }}
     >
       {children}
     </PokedexContext.Provider>
