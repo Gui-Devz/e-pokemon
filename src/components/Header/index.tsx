21855b8cac5723d7ccfda0cf1768c406b31ad1fb
Guilherme <guilherme_devz@hotmail.com>
2021-10-06 11:55:12 -0300

    feat: adding the onClick's in the nav links and adding the function when submitting the form.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
 
+import { usePokedex } from "../../hooks/usePokedex";
+
 import logoImg from "../../assets/logo.png";
 
 import { FiMapPin } from "react-icons/fi";
 import { FiSearch } from "react-icons/fi";
 
 import styles from "./header.module.scss";
+import { FormEvent, useState } from "react";
 
-export function Header() {
+interface HeaderProps {
+  onOpen: () => void;
+}
+
+export function Header({ onOpen }: HeaderProps) {
+  const { fetchPokedex, setFavoritesInPokedex, fetchPokemonByName } =
+    usePokedex();
+  const [pokemonName, setPokemonName] = useState<string>("");
+
+  const handlingSearchPokemonByName = (e: FormEvent) => {
+    e.preventDefault();
+    if (pokemonName.length > 4) {
+      fetchPokemonByName(pokemonName);
+    }
+  };
   return (
     <div className={styles.headerContainer}>
       <div className={styles.logo}>
@@ -15,9 +32,13 @@ export function Header() {
       </div>
 
       <div className={styles.searchBar}>
-        <form>
+        <form onSubmit={handlingSearchPokemonByName}>
           <div className={styles.input}>
-            <input type="text" placeholder="Search for your pokemón" />
+            <input
+              type="text"
+              onChange={(e) => setPokemonName(e.target.value)}
+              placeholder="Search for your pokemón"
+            />
             <button type="submit">
               <FiSearch color="#adadad" size="1.5em" />
             </button>
@@ -29,9 +50,20 @@ export function Header() {
         <nav>
           <ul>
             <li className={styles.cart}>
-              My cart <span>1</span>
+              <a href="#" onClick={() => fetchPokedex()}>
+                Pokedex
+              </a>
             </li>
-            <li>Favorites</li>
+            <li className={styles.cart}>
+              <a href="#" onClick={() => onOpen()}>
+                My cart <span>1</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={() => setFavoritesInPokedex()}>
+                Favorites
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
