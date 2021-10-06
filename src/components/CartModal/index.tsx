d18ec430513ed7f64cbf9fe1046d16ff4f9fc422
Guilherme <guilherme_devz@hotmail.com>
2021-10-06 11:50:59 -0300

    feat: adding the logic for opening and closing the modal

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -4,21 +4,30 @@ import { AiFillCloseCircle } from "react-icons/ai";
 
 import styles from "./cart-modal.module.scss";
 
-export function CartModal() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.cartContent}>
-        <div className={styles.closeBtn}>
-          <button>
-            <AiFillCloseCircle />
-          </button>
-        </div>
-        <div className={styles.pokemonCard}>
-          <CartPokemonCard />
-        </div>
+interface CartModalProps {
+  onClose: () => void;
+  isOpen: boolean;
+}
 
-        <button className={styles.button}>Confirm Buy</button>
-      </div>
-    </div>
+export function CartModal({ onClose, isOpen }: CartModalProps) {
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.container}>
+          <div className={styles.cartContent}>
+            <div className={styles.closeBtn}>
+              <button onClick={() => onClose()}>
+                <AiFillCloseCircle />
+              </button>
+            </div>
+            <div className={styles.pokemonCard}>
+              <CartPokemonCard />
+            </div>
+
+            <button className={styles.button}>Confirm Buy</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
