9e22ee33083819199c0b97d775f49acf574b09bc
Guilherme <guilherme_devz@hotmail.com>
2021-10-07 11:39:59 -0300

    feat: adding the functions 'resetCart', 'onClose' and 'onFinishedBuyOpen'  when the button 'finish buy' is clicked on

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -8,10 +8,16 @@ import { useCart } from "../../hooks/useCart";
 interface CartModalProps {
   onClose: () => void;
   isOpen: boolean;
+  onFinishedBuyOpen: () => void;
 }
 
-export function CartModal({ onClose, isOpen }: CartModalProps) {
-  const { cart, removePokemonInCart, updatePokemonAmount } = useCart();
+export function CartModal({
+  onClose,
+  isOpen,
+  onFinishedBuyOpen,
+}: CartModalProps) {
+  const { cart, removePokemonInCart, updatePokemonAmount, resetCart } =
+    useCart();
 
   return (
     <>
@@ -23,21 +29,34 @@ export function CartModal({ onClose, isOpen }: CartModalProps) {
                 <AiFillCloseCircle />
               </button>
             </div>
-            <div className={styles.pokemonCard}>
-              {cart &&
-                cart.map((pokemon) => {
-                  return (
-                    <div key={pokemon.name} className={styles.cardContainer}>
-                      <CartPokemonCard
-                        pokemon={pokemon}
-                        removePokemonInCart={removePokemonInCart}
-                        updatePokemonAmount={updatePokemonAmount}
-                      />
-                    </div>
-                  );
-                })}
+            <div>
+              <div className={styles.pokemonCard}>
+                {cart &&
+                  cart.map((pokemon) => {
+                    return (
+                      <div key={pokemon.name} className={styles.cardContainer}>
+                        <CartPokemonCard
+                          pokemon={pokemon}
+                          removePokemonInCart={removePokemonInCart}
+                          updatePokemonAmount={updatePokemonAmount}
+                        />
+                      </div>
+                    );
+                  })}
+              </div>
+              <button
+                className={styles.button}
+                onClick={() => {
+                  if (cart.length > 0) {
+                    resetCart();
+                    onClose();
+                    onFinishedBuyOpen();
+                  }
+                }}
+              >
+                Confirm Buy
+              </button>
             </div>
-            <button className={styles.button}>Confirm Buy</button>
           </div>
         </div>
       )}
