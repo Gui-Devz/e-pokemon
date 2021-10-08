a06827a37bb14304ec8e0647390a269472bd0c0e
Guilherme <guilherme_devz@hotmail.com>
2021-10-08 20:45:19 -0300

    feat: creating the stories for the SidedCartModal component

---
+++ src/stories/SidedCartModal.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { SidedCartModal } from "./Modals/SidedCartModal";
+
+export default {
+  title: "E-pokemon/SidedModal",
+  component: SidedCartModal,
+} as ComponentMeta<typeof SidedCartModal>;
+
+const Template: ComponentStory<typeof SidedCartModal> = (args) => (
+  <SidedCartModal {...args} />
+);
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  label: "Pokédex",
+};
